943fc1c9c2b04b2010e549b4bf3b28b544950b38
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-11-30 15:05:53 +0100

    import all the images and creato class app whit object

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import './App.css';
-import React from 'react';
 import Navbar from './Components/navbar';
 import Card from './Components/card';
+import React, { Component } from 'react';
+import california from './images/california.png';
+import dragon from './images/dragon.png';
+import dynamite from './images/dynamite.png';
+import philadelphia from './images/philadelphia.png';
+import rainbow from './images/rainbow.png';
+import shrimp from './images/shrimp.png';
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <div className='container'>
-        <h1>Cosa desideri odrinare?</h1>
-        <hr />
-        <div className='row'>
-          <Card />
+class App extends Component {
+  state = {
+    cards: [
+      { id: 0, nome: 'California', prezzo: 1.99, immagine: california, quantità: 0 },
+      { id: 1, nome: 'dragon', prezzo: 2.43, immagine: dragon, quantità: 0 },
+      { id: 2, nome: 'dynamite', prezzo: 2.49, immagine: dynamite, quantità: 0 },
+      { id: 3, nome: 'philadelphia', prezzo: 0.99, immagine: philadelphia, quantità: 0 },
+      { id: 4, nome: 'rainbow', prezzo: 2.99, immagine: rainbow, quantità: 0 },
+      { id: 5, nome: 'shrimp', prezzo: 1.49, immagine: shrimp, quantità: 0 },
+    ]
+  }
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div className='container'>
+          <h1>Cosa desideri odrinare?</h1>
+          <hr />
+          <div className='row'>
+            {this.state.cards.map(card => (
+              <Card
+                key={card.id}
+                card={card}
+              />))}
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default App;
