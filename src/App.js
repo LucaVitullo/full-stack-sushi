6d4e084e7bfef9e7fd742088d5acfa4840d1ea11
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-11-30 15:58:44 +0100

    add increment function on the app.js and on the component card

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ class App extends Component {
   state = {
     cards: [
       { id: 0, nome: 'California', prezzo: 1.99, immagine: california, quantità: 0 },
-      { id: 1, nome: 'dragon', prezzo: 2.43, immagine: dragon, quantità: 0 },
-      { id: 2, nome: 'dynamite', prezzo: 2.49, immagine: dynamite, quantità: 0 },
-      { id: 3, nome: 'philadelphia', prezzo: 0.99, immagine: philadelphia, quantità: 0 },
-      { id: 4, nome: 'rainbow', prezzo: 2.99, immagine: rainbow, quantità: 0 },
-      { id: 5, nome: 'shrimp', prezzo: 1.49, immagine: shrimp, quantità: 0 },
+      { id: 1, nome: 'Dragon', prezzo: 2.43, immagine: dragon, quantità: 0 },
+      { id: 2, nome: 'Dynamite', prezzo: 2.49, immagine: dynamite, quantità: 0 },
+      { id: 3, nome: 'Philadelphia', prezzo: 0.99, immagine: philadelphia, quantità: 0 },
+      { id: 4, nome: 'Rainbow', prezzo: 2.99, immagine: rainbow, quantità: 0 },
+      { id: 5, nome: 'Shrimp', prezzo: 1.49, immagine: shrimp, quantità: 0 },
     ]
   }
 
@@ -27,18 +27,30 @@ class App extends Component {
     const cards = this.state.cards.filter(card => card.id !== cardId);
     this.setState({ cards });
   };
+
+  handleIncrement = card => {
+    const cards = [...this.state.cards];
+    const id = cards.indexOf(card);
+    cards[id] = { ...card };
+    cards[id].quantità++;
+    this.setState({ cards });
+
+  };
+
+
   render() {
     return (
       <>
         <Navbar />
-        <div className='container'>
-          <h1>Cosa desideri odrinare?</h1>
+        <div className='container text-center'>
+          <h1>Cosa desideri ordinare?</h1>
           <hr />
           <div className='row'>
             {this.state.cards.map(card => (
               <Card
                 key={card.id}
                 onDelete={this.handleDelete}
+                onIncrement={this.handleIncrement}
                 card={card}
               />))}
           </div>
--- src/Components/card.js
+++ src/Components/card.js
@@ -5,15 +5,19 @@ class Card extends Component {
 
     render() {
         return (
-            <div className="col" >
-                <div className="card" style={{ width: '18rem', textAlign: 'center' }} >
-                    <img src={this.props.card.immagine} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{this.props.card.nome} Roll</h5>
-                        <p className="card-text">${this.props.card.prezzo}</p>
-                        <button onClick={() => this.props.onDelete(this.props.card.id)} className="btn btn-outline-danger">Elimina</button>
+            <div className="container d-flex justify-content-center col-md-6 col-lg-4 ">
+                <div className="  my-1" >
+                    <div className="card" style={{ width: '18rem', textAlign: 'center' }} >
+                        <img src={this.props.card.immagine} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{this.props.card.nome} Roll</h5>
+                            <p className="card-text">${this.props.card.prezzo}</p>
+                            <button onClick={() => this.props.onIncrement(this.props.card)} className="btn btn-primary p-1 mx-2">Aggiungi <span className="badge badge-light bg-light text-dark">{this.props.card.quantità}</span></button>
+
+                            <button onClick={() => this.props.onDelete(this.props.card.id)} className="btn btn-outline-danger">Elimina</button>
+                        </div>
                     </div>
-                </div>
+                </div >
             </div >
         );
     }
